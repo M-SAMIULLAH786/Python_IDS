ced918534e12a8fc85f91f960ba18d0ce4c5b591
Muhammad Samiullah <bsdsf24a013@pucit.edu.pk>
2025-12-27 15:30:35 +0500

    Add files via upload

--- functions.h
+++ functions.h
@@ -2,213 +2,253 @@
 #define FUNCTIONS_H
 
 #include <iostream>
+#include <stdexcept>
 using namespace std;
 
-class Edge {
-    int destination;
-    int cost;
-    Edge* next;
+template<typename T>
+class MyArray {
+private:
+    T* arr1D;
+    T** arr2D;
+    T*** arr3D;
+    int d0, d1, d2;
 
 public:
-    Edge(int dest, int c) : destination(dest), cost(c), next(nullptr) {
 
+    T* get1DArray() {
+        return arr1D;
     }
-    int getDestination() const {
-      return destination;
+    T** get2DArray() {
+        return arr2D;
     }
-    int getCost() const {
-      return cost;
+    T*** get3DArray() {
+        return arr3D;
     }
-    Edge* getNext() const {
-      return next;
-    }
-
-    void setNext(Edge* nextEdge) {
-      next = nextEdge;
-    }
-    void setDestination(int dest) {
-      destination = dest;
-    }
-};
-
-class Node {
-  private:
-    int data;
-    Node* next;
-    Edge* edges;
-    int edgeCount;
-
-public:
-    Node(int val) : data(val), next(nullptr), edges(nullptr), edgeCount(0) {}
-
-    int getData() const {
-      return data;
-    }
-    Node* getNext() const {
-      return next;
-    }
-    Edge* getEdges() const {
-      return edges;
-    }
-    int getEdgeCount() const {
-      return edgeCount;
-    }
-
-    void setNext(Node* nextNode) {
-      next = nextNode;
-    }
-    void setEdges(Edge* edgeList) {
-      edges = edgeList;
-    }
-    void incrementEdgeCount() {
-      edgeCount++;
-    }
-    void decrementEdgeCount() {
-        edgeCount--;
-    }
-    void setData(int value) {
-      data = value;
-    }
-};
-
-class Graph {
-    Node* nodes;
-    int nodeCount;
-
-public:
-    Graph() : nodes(nullptr), nodeCount(0) {
-
-    }
-    void addNode(int nodeValue) {
-        if (!hasNode(nodeValue)) {
-            Node* newNode = new Node(nodeValue);
-            newNode->setNext(nodes);
-            nodes = newNode;
-            nodeCount++;
-        }
-    }
-    void addEdge(int source, int destination, int cost) {
-        Node* sourceNode = findNode(source);
-        Node* destinationNode = findNode(destination);
-        Edge* edge;
-        edge = sourceNode->getEdges();
-        while (edge) {
-            if (edge->getDestination() == destination)
-              return;
-            edge = edge->getNext();
-        }
-        Edge* newEdge;
-        newEdge = new Edge(destination, cost);
-        newEdge->setNext(sourceNode->getEdges());
-        sourceNode->setEdges(newEdge);
-        sourceNode->incrementEdgeCount();
-    }
-    int getEdgeCost(int source, int destination) const {
-        Node* sourceNode = findNode(source);
-        if (sourceNode) {
-            Edge* edge = sourceNode->getEdges();
-            while (edge) {
-                if (edge->getDestination() == destination) return edge->getCost();
-                edge = edge->getNext();
+    MyArray(int d0, int d1 = 0, int d2 = 0) : d0(d0), d1(d1), d2(d2), arr1D(0), arr2D(0), arr3D(0) {
+        if (d1 == 0 && d2 == 0) {
+            arr1D = new T[d0];
+        } else if (d2 == 0) {
+            arr2D = new T*[d1];
+            for (int m = 0; m < d1; m++) {
+                arr2D[m] = new T[d0];
             }
-        }
-        return -1;
-    }
-    int getEdgeCountForNode(int nodeValue) const {
-        Node* node;
-        node = findNode(nodeValue);
-        if (node) {
-            return node->getEdgeCount();
         } else {
-            return -1;
-        }
-    }
-    int getNodeCount() const {
-      return nodeCount;
-    }
-    void updateNode(int oldValue, int newValue) {
-        if (hasNode(newValue)){
-          return;
-        }
-        Node* node = findNode(oldValue);
-        if (node) {
-            node->setData(newValue);
-            Node* currentNode = nodes;
-            while (currentNode) {
-                Edge* edge = currentNode->getEdges();
-                while (edge) {
-                    if (edge->getDestination() == oldValue) edge->setDestination(newValue);
-                    edge = edge->getNext();
+            arr3D = new T**[d2];
+            for (int n = 0; n < d2; n++) {
+                arr3D[n] = new T*[d1];
+                for (int m = 0; m < d1; m++) {
+                    arr3D[n][m] = new T[d0];
                 }
-                currentNode = currentNode->getNext();
             }
         }
     }
-    void deleteNode(int nodeValue) {
-        Node* prevNode = nullptr;
-        Node* currentNode = nodes;
-        while (currentNode && currentNode->getData() != nodeValue) {
-            prevNode = currentNode;
-            currentNode = currentNode->getNext();
-        }
-        if (!currentNode){
-          return;
-          }
 
-        if (prevNode) {
-            prevNode->setNext(currentNode->getNext());
-        } else {
-            nodes = currentNode->getNext();
-        }
-        Edge* edge = currentNode->getEdges();
-        while (edge) {
-            Edge* tempEdge = edge;
-            edge = edge->getNext();
-            delete tempEdge;
-        }
-        delete currentNode;
-        nodeCount--;
-        Node* otherNode = nodes;
-        while (otherNode) {
-            deleteEdge(otherNode->getData(), nodeValue);
-            otherNode = otherNode->getNext();
-        }
-    }
-    bool hasNode(int nodeValue) const {
-      return findNode(nodeValue) != nullptr;
-    }
-    void deleteEdge(int source, int destination) {
-        Node* sourceNode = findNode(source);
-        if (!sourceNode){
-          return;
-          }
-        Edge* prevEdge = nullptr;
-        Edge* currentEdge = sourceNode->getEdges();
-        while (currentEdge && currentEdge->getDestination() != destination) {
-            prevEdge = currentEdge;
-            currentEdge = currentEdge->getNext();
-        }
-        if (currentEdge) {
-            if (prevEdge) {
-                prevEdge->setNext(currentEdge->getNext());
-            } else {
-                sourceNode->setEdges(currentEdge->getNext());
+    // Destructor
+    ~MyArray() {
+        if (arr1D) {
+            delete[] arr1D;
+        } else if (arr2D) {
+            for (int m = 0; m < d1; m++) {
+                delete[] arr2D[m];
             }
-            delete currentEdge;
-            sourceNode->decrementEdgeCount();
+            delete[] arr2D;
+        } else if (arr3D) {
+            for (int n = 0; n < d2; n++) {
+                for (int m = 0; m < d1; m++) {
+                    delete[] arr3D[n][m];
+                }
+                delete[] arr3D[n];
+            }
+            delete[] arr3D;
         }
     }
-    Node* findNode(int value) const {
-        Node* current = nodes;
-        while (current) {
-            if (current->getData() == value){
-              return current;
-              }
-            current = current->getNext();
+
+    // Create method
+    static MyArray* create(int d0 = 0, int d1 = 0, int d2 = 0) {
+        return new MyArray(d0, d1, d2);
+    }
+
+    // Set element
+    void setElement(T value, int index0, int index1 = 0, int index2 = 0) {
+        if (arr1D) {
+            if (index0 >= d0) {
+                cout << "Index out of bound";
+                return;
+            }
+            arr1D[index0] = value;
+        } else if (arr2D) {
+            if (index1 >= d1 || index0 >= d0) {
+                cerr << "Index out of bounds";
+                return;
+            }
+            arr2D[index1][index0] = value;
+        } else if (arr3D) {
+            if (index2 >= d2 || index1 >= d1 || index0 >= d0) {
+                cerr << "Index out of bounds";
+                return;
+            }
+            arr3D[index2][index1][index0] = value;
         }
-        return nullptr;
+    }
+
+    // Get element
+    T getElement(int index0, int index1 = 0, int index2 = 0) const {
+        if (arr1D) {
+            if (index0 >= d0) {
+                cout << "Index out of bounds";
+                return T();
+            }
+            return arr1D[index0];
+        } else if (arr2D) {
+            if (index1 >= d1 || index0 >= d0) {
+                cout << "Index out of bounds!";
+                return T();
+            }
+            return arr2D[index1][index0];
+        } else if (arr3D) {
+            if (index2 >= d2 || index1 >= d1 || index0 >= d0) {
+                cout << "Index out of bounds!";
+                return T();
+            }
+            return arr3D[index2][index1][index0];
+        }
+        return T();
+    }
+
+    // Get dimensions
+    int getDimensions() const {
+        if (arr1D) {
+            return 1;
+        } else if (arr2D) {
+            return 2;
+        } else if (arr3D) {
+            return 3;
+        }
+        return 0;
+    }
+
+    // Display array
+    void display() const {
+        if (arr1D) {
+            for (int l = 0; l < d0; l++) {
+                cout << arr1D[l] << "\t";
+            }
+            cout << endl;
+        } else if (arr2D) {
+            for (int m = 0; m < d1; m++) {
+                for (int l = 0; l < d0; l++) {
+                    cout << arr2D[m][l] << "\t";
+                }
+                cout << endl;
+            }
+        } else if (arr3D) {
+            for (int n = 0; n < d2; n++) {
+                cout << "Layer " << n + 1 << ":\n";
+                for (int m = 0; m < d1; m++) {
+                    for (int l = 0; l < d0; l++) {
+                        cout << arr3D[n][m][l] << " ";
+                    }
+                    cout << endl;
+                }
+            }
+        }
+    }
+    T* getRow3D(int r, int l) const {
+        if (arr3D && r < d1 && l < d2) {
+            return arr3D[l][r];
+        }
+    }
+    void radixSort1D(T* array, int size) {
+        T maxval = array[0];
+        for (int l = 1; l < size; l++) {
+            if (array[l] > maxval) {
+                maxval = array[l];
+            }
+        }
+        int maxDigits = 0;
+        while (maxval > 0) {
+            maxDigits++;
+            maxval /= 10;
+        }
+        T* result = new T[size];
+
+        int exponent = 1;
+        for (int d = 0; d < maxDigits; d++) {
+            int arr[10] = {0};
+            for (int l = 0; l < size; l++) {
+                int digit = (array[l] / exponent) % 10;
+                arr[digit]++;
+            }
+            for (int l = 1; l < 10; l++) {
+                arr[l] += arr[l - 1];
+            }
+            for (int l = size - 1; l >= 0; l--) {
+                int digit = (array[l] / exponent) % 10;
+                result[--arr[digit]] = array[l];
+            }
+            for (int l = 0; l < size; l++) {
+                array[l] = result[l];
+            }
+            exponent *= 10;
+        }
+        delete[] result;
+    }
+void countSort1D(T* array, int size) {
+    T maxVal = array[0], minVal = array[0];
+    for (int l = 1; l < size; l++) {
+        if (array[l] > maxVal) maxVal = array[l];
+        if (array[l] < minVal) minVal = array[l];
+    }
+    int range = maxVal - minVal + 1;
+    int* count = new int[range] {0};
+    for (int l = 0; l < size; l++) {
+        count[array[l] - minVal]++;
+    }
+
+    for (int l = 1; l < range; l++) {
+        count[l] += count[l - 1];
+    }
+    T* output = new T[size];
+    for (int l = size - 1; l >= 0; l--) {
+        count[array[l] - minVal]--;
+        int index = count[array[l] - minVal];
+        output[index] = array[l];
+
+    }
+    for (int l = 0; l < size; l++) {
+        array[l] = output[l];
+    }
+
+    delete[] count;
+    delete[] output;
+}
+
+    void radixSort2D() {
+        for (int m = 0; m < d1; m++) {
+            radixSort1D(arr2D[m], d0);
+        }
+    }
+    void countSort2D() {
+        for (int m = 0; m < d1; m++) {
+            countSort1D(arr2D[m], d0);
+        }
+    }
+
+    void radixSort3D() {
+        for (int n = 0; n < d2; n++) {
+            for (int m = 0; m < d1; m++) {
+                radixSort1D(arr3D[n][m], d0);
+            }
+        }
+    }
+
+    void countSort3D() {
+        for (int n = 0; n < d2; n++) {
+            for (int m = 0; m < d1; m++) {
+                countSort1D(arr3D[n][m], d0);
+            }
+        }
     }
 };
-
 
 #endif
